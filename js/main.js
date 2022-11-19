ef172602b3ac21519488b5b08f3bdc34d2b1d522
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-19 19:06:40 +0100

    Esercizio Completo

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
 // Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente.
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+let mioArray = [];
+
 // 4) Andiamo a collegare il nostro bottone.
 
 let myButton = document.getElementById("mystart");
@@ -12,17 +14,42 @@ let myButton = document.getElementById("mystart");
 
 let myShowNumber = document.getElementById("show-number");
 
-// // 6) Andiamo a creare una classe "hidden"
+// 6) Andiamo ad aggiungere una classe "hidden".
 
 myShowNumber.classList.add("hidden");
 
-// 7) Andiamo a collegare al click il nostro bottone.
+// 7) Andiamo a collegare il "text".
+
+let numeri = document.getElementById("text");
+
+// 8) Andiamo ad aggiungere una classe "hidden".
+
+numeri.classList.add("hidden");
+
+// 9) Andiamo a collegare il bottone "invia".
+
+let myInvia = document.getElementById("invia");
+
+// 10) Andiamo ad aggiungere una classe "hidden".
+
+myInvia.classList.add("hidden");
+
+// 11) Andiamo a collegare il bottone "invia".
+
+let myOutput = document.getElementById("output");
+
+// 12) Andiamo ad aggiungere una classe "hidden".
+
+myOutput.classList.add("hidden");
+
+// 13) Andiamo a collegare al click il nostro bottone.
 
 myButton.addEventListener("click",
 
     function(){
 
         myShowNumber.classList.remove("hidden");
+        myOutput.innerHTML = '';
 
         // 3)Andiamo a creare un array di 100 numeri includendo i 5 numeri che ci serivranno.
 
@@ -31,35 +58,56 @@ myButton.addEventListener("click",
 
         myShowNumber.innerHTML = myArrayFiveNumbers;
 
-        // 8) Andiamo a inserire il nostro "array" nel ciclo "for" e andiamoa creare una funzione per il  "timer" di "30 sec" per i nostri numeri.
+        // 14) Andiamo a inserire il nostro "array" nel ciclo "for" e andiamoa creare una funzione per il  "timer" di "30 sec" per i nostri numeri.
 
-        let trentaSec = 3 ;
+        let trentaSec = 1 ;
 
         let timer = setInterval(function(){
-            if(trentaSec === -1){
+            if(trentaSec === 0){
                 myShowNumber.innerHTML = '';
-                myShowNumber.classList.add("hidden");
                 clearInterval(timer);
                 myShowNumber.classList.remove("hidden");
-
-                let mioArray =[];
-
-                for (let i = 0; i < myArrayFiveNumbers.length; i++) {
-                    let numeri = parseInt(prompt(`Inserisci i numeri`));
-                    if(myArrayFiveNumbers.includes(numeri)){
-                        mioArray.push(numeri);
-                        console.log(`Hai indovinato ${mioArray.lenght} numeri`);
-                    }else{
-                        console.log("Non hai indovinato nesseun numero");
-                    }
-                // myShowNumber.append(numeri);
-                // console.log(numeri);
-                }
-            }else
+                numeri.classList.remove("hidden");
+                myInvia.classList.remove("hidden");
+            }else{
                 trentaSec--;
                 console.log(trentaSec);
-
+            }
         }, 1000)
+
+        // 15) Andiamo a collegare al click il nostro bottone "Invia".
+
+        let mioArray = [];
+
+        myInvia.addEventListener("click",
+
+            function(){
+
+            mioArray.push(numeri.value);
+            console.log(mioArray);
+
+            myShowNumber.innerHTML = `${mioArray}`;
+
+            if(5 === mioArray.length){
+            
+                for (let i = 0; i < myArrayFiveNumbers.length; i++) {
+            
+                    if(myArrayFiveNumbers[i] == mioArray[i]){
+                        console.log("il numeri sono corretti");
+                        numeri.classList.add("hidden");
+                        myInvia.classList.add("hidden");
+                        myOutput.classList.remove("hidden");
+                        document.getElementById("output").innerHTML = `Hai indovinato i numeri`;
+                    }else{
+                        console.log("il numeri non sono corretti");
+                        numeri.classList.add("hidden");
+                        myInvia.classList.add("hidden");
+                        myOutput.classList.remove("hidden");
+                        document.getElementById("output").innerHTML = `Non hai indovinato i numeri`;
+                    }
+                }
+            }
+        })
     }
 )
 
